a9e4f5ab07cdd8ba8a653a5eb156c2cb5782d820
GodwinGbor <goddeyg45@gmail.com>
2023-09-01 17:47:56 -0700

    my C - Bit Manipulation Project

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - printing the binary equivalent to the decimal numbers
+ * @n: binary number to be printed
+ */
+void print_binary(unsigned long int n)
+{
+	int j, count = 0;
+	unsigned long int current;
+
+	for (j = 63; j >= 0; j--)
+	{
+		current = n >> j;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+my project
+0x14. C - Bit manipulation
